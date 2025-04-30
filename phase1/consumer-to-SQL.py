e5d35cf923c69c5cfc4e284394b2c21af759fd79
jwheller0013 <jwheller0013@yahoo.com>
2025-04-30 09:28:26 -0400

    phase1 complete

--- phase1/consumer-to-SQL.py
+++ phase1/consumer-to-SQL.py
@@ -1,55 +1,47 @@
-from kafka import KafkaConsumer, TopicPartition
+from kafka import KafkaConsumer
 from json import loads
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from datetime import datetime
 
-ledger = SQLAlchemy()
+db = SQLAlchemy()
 
-def create_app():    
+def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.ledger'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    ledger.init_app(app)
+    db.init_app(app)
 
-    if not path.exists('database.ledger'):
+    if not path.exists('database.db'):
         with app.app_context():
-            ledger.create_all()
+            db.create_all()
 
     return app
 
-class Transaction(ledger_db.Model):
+class Transaction(db.Model):
     __tablename__ = 'transactions'
 
-    id = ledger_db.Column(ledger_db.Integer, primary_key=True)
-    custid = ledger_db.Column(ledger_db.Integer)
-    type = ledger_db.Column(ledger_db.String(3))
-    date = ledger_db.Column(ledger_db.DateTime)
-    amt = ledger_db.Column(ledger_db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    custid = db.Column(db.Integer)
+    type = db.Column(db.String(3))
+    date = db.Column(db.DateTime)
+    amt = db.Column(db.Integer)
 
     def __repr__(self):
         return f"<Transaction(custid={self.custid}, type='{self.type}', date='{self.date}', amt={self.amt})>"
 
 class XactionConsumer:
-    def __init__(self):
+    def __init__(self, app):
+        self.app = app
         self.consumer = KafkaConsumer('bank-customer-events',
             bootstrap_servers=['localhost:9092'],
-            # auto_offset_reset='earliest',
             group_id='transaction-consumer-group-sqlite',
             value_deserializer=lambda m: loads(m.decode('ascii')))
-        ## These are two python dictionarys
-        # Ledger is the one where all the transaction get posted
         self.ledger = {}
-        # custBalances is the one where the current blance of each customer
-        # account is kept.
-        self.custBalances = {}
-        # THE PROBLEM is every time we re-run the Consumer, ALL our customer
-        # data gets lost!
-        # add a way to connect to your database here.
+        self.custBalances = self.load_customer_balances()
 
-        #Go back to the readme.
     def load_customer_balances(self):
-        """Loads customer balances from the database."""
         balances = {}
         with self.app.app_context():
             try:
@@ -67,7 +59,6 @@ class XactionConsumer:
         return balances
 
     def store_transaction(self, message):
-        """Stores a transaction in the database."""
         with self.app.app_context():
             try:
                 timestamp = datetime.fromtimestamp(message['date'])
@@ -77,11 +68,11 @@ class XactionConsumer:
                     date=timestamp,
                     amt=message['amt']
                 )
-                ledger_db.session.add(db_transaction)
-                ledger_db.session.commit()
+                db.session.add(db_transaction)
+                db.session.commit()
                 print(f"Transaction stored in DB: {db_transaction}")
             except Exception as e:
-                ledger_db.session.rollback()
+                db.session.rollback()
                 print(f"Error storing transaction in DB: {e}")
 
     def handleMessages(self):
@@ -90,7 +81,6 @@ class XactionConsumer:
             print('{} received'.format(message))
             self.ledger[message['custid']] = message
             self.store_transaction(message)
-            # add message to the transaction table in your SQL usinf SQLalchemy
             if message['custid'] not in self.custBalances:
                 self.custBalances[message['custid']] = 0
             if message['type'] == 'dep':
