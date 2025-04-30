04f7e8e4847c5e80a507a85c4b5d33e75435a674
jwheller0013 <jwheller0013@yahoo.com>
2025-04-29 20:05:14 -0400

    A start

--- phase1/consumer-to-SQL.py
+++ phase1/consumer-to-SQL.py
@@ -1,5 +1,15 @@
 from kafka import KafkaConsumer, TopicPartition
 from json import loads
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+ledger = SQLAlchemy()
+
+def create_app():    
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    ledger.init_app(app)
 
 class XactionConsumer:
     def __init__(self):
